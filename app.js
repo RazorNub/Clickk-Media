b32a5e1d41645fa8e603575754484a80c415308a
Vedant Yaduvanshi-) <87130565+vedantyaduvanshi@users.noreply.github.com>
2024-02-28 14:21:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+function redirectToURL() {
+    var urlToRedirect = "https://calendly.com/clickkmedia/30min?fbclid=PAAaaV-CAq3Kql6HbhGflDZH9j9cGIfkL0IaUDJGqpORKSvgAh9HQnR4t2M_E&month=2024-02"; // Replace with your desired URL
+    window.location.href = urlToRedirect;
+}
+
+// Get all elements with the specified class
+var elements = document.getElementsByClassName("book-call");
+
+// Attach the click event to each element
+for (var i = 0; i < elements.length; i++) {
+    elements[i].onclick = redirectToURL;
+}
+
+
+function click1(){
+    window.open("https://www.youtube.com/@JamesyMCYT")
+   }
+
+   
+function click2(){
+    window.open("https://www.youtube.com/@dreamsofcode/featured")
+   }
+
+   
+function click3(){
+    window.open("https://www.youtube.com/@BlushhYT")
+   }
+
+   
+function click4(){
+    window.open("https://www.youtube.com/@ByteNotes")
+   }
